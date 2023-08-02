a0ba3c5e5dcb603f0007584f843d8cdd6cfce4b4
karan <kskular05@gmail.com>
2023-08-02 19:15:26 +0530

    GUI added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let playerWins = 0;
+let computerWins = 0;
+let round_num = 1;
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)
-    if (choice == 0){
+    if (choice == 0){    
         return "rock"
     }
     else if(choice == 1){
@@ -11,7 +15,8 @@ function getComputerChoice(){
     }
 }
 
-function play(playerChoice, computerChoice, round_num){
+function play(playerChoice){
+    computerChoice = getComputerChoice();
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == computerChoice){
         console.log(`No one wins Round ${round_num}, the player and computer both chose ${computerChoice}, the same round will start again`);
@@ -22,9 +27,9 @@ function play(playerChoice, computerChoice, round_num){
         case "rock":
             winner = computerChoice == "scissors" ? "player" : "computer";
         case "paper":
-            winner = computerChoice == "rock" ? "player" : "computer";
+            winner = computerChoice == "scissors" ? "player" : "computer";
         case "scissors":
-            winner = computerChoice == "paper" ? "player" : "computer";     
+            winner = computerChoice == "rock" ? "player" : "computer";     
     }
     let winnerchoice;
     let loserchoice;
@@ -40,26 +45,26 @@ function play(playerChoice, computerChoice, round_num){
     return winner == "player" ? 1 : 2;
 }
 
-function game(max_rounds){
-    let playerWins = 0;
-    let computerWins = 0
-    let round_num = 1;
-    for(let i = 0; i < max_rounds; i++){
-        x = play(prompt(), getComputerChoice(), round_num)
-        if (x == 0){
-            i -= 1;
-            continue;
-        }
-        else if (x == 1){
-            playerWins += 1;
-            round_num += 1;
-        }
-        else{
-            computerWins += 1;
-            round_num += 1;
-        }
-
+function game(playerChoice){
+    x = play(playerChoice)
+    if (x == 0){
+        return;
     }
-    console.log(`The overall winner is ${computerWins > playerWins ? "computer" : "player"} with ${Math.max(computerWins, playerWins)} to ${Math.min(playerWins, computerWins)} wins`)
+    else if (x == 1){
+        playerWins += 1;
+        round_num += 1;
+    }
+    else{
+        computerWins += 1;
+        round_num += 1;
+    }
 }
-game(3)
+
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {game("rock")});
+paper.addEventListener("click", () => {game("paper")});
+scissors.addEventListener("click", () => {game("scissors")});
